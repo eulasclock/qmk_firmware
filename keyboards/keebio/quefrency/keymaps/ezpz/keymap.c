200564adf86dc5a693eece989b8cc6363b05d3a8
eulasclock <eula1998@gmail.com>
2021-12-14 21:41:01 -0800

    Adjusted knob speed and right macro keymapping

--- keyboards/keebio/quefrency/keymaps/ezpz/keymap.c
+++ keyboards/keebio/quefrency/keymaps/ezpz/keymap.c
@@ -16,8 +16,8 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_65_with_macro(
     KC_MEDIA_PLAY_PAUSE,   KC_GESC, KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC, KC_WWW_BACK, \
-    KC_MEDIA_PREV_TRACK,   KC_MEDIA_NEXT_TRACK,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_BRIGHTNESS_UP, \
-    KC_MEDIA_PLAY_PAUSE,   KC_MUTE,   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_BRIGHTNESS_DOWN, \
+    KC_MEDIA_PREV_TRACK,   KC_MEDIA_NEXT_TRACK,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP, \
+    KC_MEDIA_PLAY_PAUSE,   KC_MUTE,   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGDN, \
     KC_BRIGHTNESS_DOWN,   KC_BRIGHTNESS_UP,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PRINT_SCREEN, \
     KC_NO,   KC_NO,  KC_LCTL, KC_LGUI, KC_LALT, MO(_FN1),KC_SPC,           MO(_FN1),KC_SPC,  MO(_FN1), KC_RCTL, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
   ),
